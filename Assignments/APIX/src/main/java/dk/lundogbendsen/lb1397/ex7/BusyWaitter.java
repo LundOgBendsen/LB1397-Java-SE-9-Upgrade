d70f572a5eb8400b3ef79f7f3ff7509a6c27f9c7
Thomas Borg Salling <tbsalling@tbsalling.dk>
2017-09-18 09:53:13 +0200

    Added ex7

---
+++ Assignments/APIX/src/main/java/dk/lundogbendsen/lb1397/ex7/BusyWaitter.java
@@ -0,0 +1,22 @@
+package dk.lundogbendsen.lb1397.ex7;
+
+public class BusyWaitter {
+
+    public static void main(String[] args) {
+        busyWaitWithoutSpinWaitHints();
+        // busyWaitWithSpinWaitHints();
+    }
+
+    private static void busyWaitWithoutSpinWaitHints() {
+        System.out.println("Starting busy wait without CPU spin-wait hints");
+
+        // TODO
+    }
+
+    private static void busyWaitWithSpinWaitHints() {
+        System.out.println("Starting busy wait with CPU spin-wait hints");
+
+       // TODO
+    }
+
+}
---
+++ Assignments/APIX/src/main/java/dk/lundogbendsen/lb1397/ex7/solution/BusyWaitter.java
@@ -0,0 +1,25 @@
+package dk.lundogbendsen.lb1397.ex7.solution;
+
+public class BusyWaitter {
+
+    public static void main(String[] args) {
+        //busyWaitWithoutSpinWaitHints();
+        busyWaitWithSpinWaitHints();
+    }
+
+    private static void busyWaitWithoutSpinWaitHints() {
+        System.out.println("Starting busy wait without CPU spin-wait hints");
+
+        while(true) {
+        }
+    }
+
+    private static void busyWaitWithSpinWaitHints() {
+        System.out.println("Starting busy wait with CPU spin-wait hints");
+
+        while(true) {
+            Thread.onSpinWait();
+        }
+    }
+
+}
