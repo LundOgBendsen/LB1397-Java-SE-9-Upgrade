0cab263550520fa30be3d7e50ce601d87af623db
Thomas Borg Salling <tbsalling@tbsalling.dk>
2017-09-18 09:55:44 +0200

    Rename (remove double-t)

--- Assignments/APIX/src/main/java/dk/lundogbendsen/lb1397/ex7/BusyWaiter.java
+++ Assignments/APIX/src/main/java/dk/lundogbendsen/lb1397/ex7/BusyWaiter.java
@@ -1,25 +1,22 @@
-package dk.lundogbendsen.lb1397.ex7.solution;
+package dk.lundogbendsen.lb1397.ex7;
 
-public class BusyWaitter {
+public class BusyWaiter {
 
     public static void main(String[] args) {
-        //busyWaitWithoutSpinWaitHints();
-        busyWaitWithSpinWaitHints();
+        busyWaitWithoutSpinWaitHints();
+        // busyWaitWithSpinWaitHints();
     }
 
     private static void busyWaitWithoutSpinWaitHints() {
         System.out.println("Starting busy wait without CPU spin-wait hints");
 
-        while(true) {
-        }
+        // TODO
     }
 
     private static void busyWaitWithSpinWaitHints() {
         System.out.println("Starting busy wait with CPU spin-wait hints");
 
-        while(true) {
-            Thread.onSpinWait();
-        }
+       // TODO
     }
 
 }
--- Assignments/APIX/src/main/java/dk/lundogbendsen/lb1397/ex7/solution/BusyWaiter.java
+++ Assignments/APIX/src/main/java/dk/lundogbendsen/lb1397/ex7/solution/BusyWaiter.java
@@ -1,22 +1,25 @@
-package dk.lundogbendsen.lb1397.ex7;
+package dk.lundogbendsen.lb1397.ex7.solution;
 
-public class BusyWaitter {
+public class BusyWaiter {
 
     public static void main(String[] args) {
-        busyWaitWithoutSpinWaitHints();
-        // busyWaitWithSpinWaitHints();
+        //busyWaitWithoutSpinWaitHints();
+        busyWaitWithSpinWaitHints();
     }
 
     private static void busyWaitWithoutSpinWaitHints() {
         System.out.println("Starting busy wait without CPU spin-wait hints");
 
-        // TODO
+        while(true) {
+        }
     }
 
     private static void busyWaitWithSpinWaitHints() {
         System.out.println("Starting busy wait with CPU spin-wait hints");
 
-       // TODO
+        while(true) {
+            Thread.onSpinWait();
+        }
     }
 
 }
