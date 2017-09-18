8fe30af6de100570b893da5fdfac75bb43e5b50e
Thomas Borg Salling <tbsalling@tbsalling.dk>
2017-09-18 08:53:13 +0200

    Folders reorganized

--- Assignments/APIX/src/main/java/dk/lundogbendsen/lb1397/logging/opgave/LBLoggerFinder.java
+++
@@ -1,8 +0,0 @@
-package dk.lundogbendsen.lb1397.logging.opgave;
-
-public class LBLoggerFinder { // TODO
-
-    public System.Logger getLogger(String name, Module module) {
-        return null; // TODO
-    }
-}
